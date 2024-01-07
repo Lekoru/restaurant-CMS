d00b4a923b9712b6a8ad3d624dc6a22114be3baf
Adrian Pietrowski <56158556+Lekoru@users.noreply.github.com>
2024-01-07 16:00:26 +0100

    Adjusted userRoutes to use jwt tokens, and encrypted passwords

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,27 +1,47 @@
 import {Router} from "express"
 import db from "../db.js"
 import {Op} from "sequelize";
+import jwt from "jsonwebtoken";
+import securePassword from "secure-random-password";
+import bcrypt from "bcryptjs"
 
 const router = Router()
 const {users} = db.models
 
+const hassPassword = async (password) => {
+  const salt = await bcrypt.genSalt()
+  const hashPass = await bcrypt.hash(password, salt)
+  return hashPass
+}
+
+const authenticateToken = async (req, res) => {
+  const token = req.header('auth-token')
+  if (!token) return res.json({ message: 'Token not found.' })
+  const verified = jwt.verify(token, process.env.JWT_SECRET)
+  if (!verified) return res.json({ message: 'Invalid token verification.' })
+  const _user = await users.findOne({ where: { user_id: verified.id }})
+  if (!_user) return res.status(400).json({ message: 'User doesn\'t exist' })
+  return _user
+}
+
 router.post("/createUser", async (req, res) => {
-const {userEmail, name, email, pass, role} = req.body
-let user
+const {name, email, pass, role} = req.body
+let user = await authenticateToken(req, res)
 try {
-  if (!userEmail) res.status(400).json({message: "Admin email not specified."})
-  user = await users.findOne({where: {Email: userEmail}})
-  if(!user) res.status(400).json({message: "Creator doesn't exist."})
-  if(user.Role !== "Admin") res.status(400).json({message: "You don't have permission."})
-  if(!name) res.status(400).json({message: "Name not entered."})
-  if(!email) res.status(400).json({message: "Email not entered."})
-  if(!pass) res.status(400).json({message: "Password not entered."})
-  if(!role) res.status(400).json({message: "Role not entered."})
+  if(user.Role !== "Admin") return res.status(400).json({message: "You don't have permission."})
+  user = await users.findOne({where: {Email: email}})
+  if (user) return res.status(400).json({message: "User with this email already exist."})
+  if(!name) return res.status(400).json({message: "Name not entered."})
+  if(!email) return res.status(400).json({message: "Email not entered."})
+  if(!pass) return res.status(400).json({message: "Password not entered."})
+  if(!role) return res.status(400).json({message: "Role not entered."})
+
+  const hashedPass = await hassPassword(pass)
 
   const newUser = await users.create({
     Name: name,
     Email: email,
-    Password: pass,
+    Password: hashedPass,
     Role: role
   })
 
@@ -34,24 +54,25 @@ try {
 
 router.post("/login", async (req, res) => {
   const {email, password} = req.body
-  let user;
   try {
-    //Check if user entered the email
-    if (!email) res.status(400).json({message: "Missing email."})
-    user = await users.findOne({where: {Email: email}})
-    if (!user) res.status(400).json({message: "User doesn't exist."})
-    //Check if user entered the password
-    if (!password) res.status(400).json({message: "Missing password."})
+    if (!email) return res.status(400).json({message: "Missing email."})
+    const user = await users.findOne({where: {Email: email}})
+    if (!user) return res.status(400).json({message: "User doesn't exist."})
+    if (!password) return res.status(400).json({message: "Missing password."})
 
-    user = await users.findOne({where: {Email: email, Password: password}})
-    if (!user) res.status(400).json({message: "Email or password incorrect."})
+    if (!await bcrypt.compare(password, user.Password))
+      return res.status(400).json({message: 'Invalid password.'})
 
-    return res.status(200).json({user: {
-      id: user.id,
-      Name: user.Name,
-      Email: user.Email,
-      Role: user.Role
-    }})
+    const token = jwt.sign({id: user.Email}, process.env.JWT_SECRET)
+    return res.status(200).json({
+      token,
+      user: {
+        id: user.id,
+        Name: user.Name,
+        Email: user.Email,
+        Role: user.Role
+      }
+    })
   } catch (e) {
     console.error(e)
     return res.status(500).json({error: e.message})
@@ -59,21 +80,27 @@ router.post("/login", async (req, res) => {
 })
 
 router.delete("/deleteUser", async (req, res) => {
-  const {email, userToDelete} = req.body
-  let user
   const transaction = await db.transaction()
+  const {userToDelete} = req.body
   try {
-      if (!userToDelete) res.status(400).json({message: "User to delete not specified."})
-      user = await users.findOne({where: {Email: email}})
-      if (!user) res.status(400).json({message: "Admin doesn't exist."})
-      if (user.Role !== "Admin") res.status(400).json({message: "User don't have permission."})
-      user = await users.findOne({where: {Email: userToDelete}})
-      if (!user) res.status(400).json({message: "User to delete doesn't exist."})
-
-      await user.destroy()
+    let user = await authenticateToken(req, res)
+    if (user.Role !== "Admin") {
+      await transaction.rollback()
+      return res.status(400).json({message: "User don't have permission."})
+    }
+    // Overwrite user => User to delete
+    user = await users.findOne({where: {Email: userToDelete}, transaction})
+    if (!user) {
+      await transaction.rollback()
+      return res.status(400).json({message: "User to delete not specified."})
+    }
+    if (!user ) {
+      await transaction.rollback()
+      return res.status(400).json({message: "User to delete doesn't exist."})
+    }
+      await user.destroy(transaction)
       await transaction.commit()
       return res.status(200).json({message: "User deleted successfully."})
-
     } catch (e) {
       await transaction.rollback()
       console.error(e)
@@ -82,37 +109,36 @@ router.delete("/deleteUser", async (req, res) => {
 })
 
 router.patch("/changePassword", async (req, res) => {
-  const {email, oldPassword, newPassword} = req.body
-  let user
-    const transaction = await db.transaction()
-    try {
+  const transaction = await db.transaction()
+  const {oldPassword, newPassword} = req.body
+  try {
+    const user = await authenticateToken(req, res)
       if (!oldPassword && !newPassword){
         await transaction.rollback()
         return res.status(400).json({error: "Invalid input. Please provide old password, and new password."})
       }
-      if(!email) {
-        await transaction.rollback()
-        return res.status(400).json({error: "Email not entered."})
-      }
-      user = await users.findOne({where: {Email: email}, transaction})
       if(!user) {
         await transaction.rollback()
         return res.status(400).json({error: "User doesn't exist."})
       }
-      if (user.Password !== oldPassword) {
-        await transaction.rollback()
-        return res.status(400).json({error: "Entered wrong old password."})
-      }
+
       if(!oldPassword) {
         await transaction.rollback()
         return res.status(400).json({error: "Old password not entered."})
       }
+
+      if (!await bcrypt.compare(oldPassword, user.Password)) {
+        await transaction.rollback()
+        return res.status(400).json({message: 'Invalid old password.'})
+      }
+
       if(!newPassword) {
         await transaction.rollback()
         return res.status(400).json({error: "New password not entered."})
       }
 
-      await user.update({Password: newPassword}, {transaction})
+      const hashedPass = await hassPassword(newPassword)
+      await user.update({Password: hashedPass}, {transaction})
       await transaction.commit()
       res.status(200).json({message: "Password successfully changed."})
     } catch (e) {
@@ -121,21 +147,21 @@ router.patch("/changePassword", async (req, res) => {
       return res.status(500).json({error: e.message})
     }
 })
-
 router.get("/getUsers", async (req, res) => {
-  const {email} = req.body
-    let user
-    try {
-      if(!email) res.status(400).json({message: "Email not entered."})
-      user = await users.findOne({where: {Email: email}})
-      if(!user) res.status(400).json({message: "User doesn't exist."})
-      if(user.Role !== "Admin") res.status(400).json({message: "You don't have permissions."})
-      const usersList = await users.findAll({where: {Email: {[Op.not]: email}}})
-      usersList.map((user) => {
-        user.Password = undefined
-        return user
-      })
-      res.status(200).json({usersList})
+  const user = await authenticateToken(req, res)
+  try {
+    if(user.Role !== "Admin") res.status(400).json({message: "You don't have permissions."})
+    const usersList = await users.findAll({where: {Email: {[Op.not]: user.Email}}})
+    usersList.map((user) => {
+      user.Password = undefined
+      return user
+    })
+    res.status(200).json({usersList})
+  } catch (e) {
+      console.error(e)
+      return res.status(500).json({error: e.message})
+  }
+})
 router.patch("/genUserPassword", async (req, res) => {
   const transaction = await db.transaction()
   const {userToGenPass} = req.body
