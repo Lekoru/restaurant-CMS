6c5ae4e0e2476b7c6c5664ad253be94019da6c23
Adrian Pietrowski <56158556+Lekoru@users.noreply.github.com>
2024-01-08 21:06:39 +0100

    Made main photo/title/description and restaurant description load from backend

--- frontend/src/components/Index/Wrapper.tsx
+++ frontend/src/components/Index/Wrapper.tsx
@@ -1,41 +1,40 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import {webSettingsProps} from "../../views";
 
-function Wrapper() {
-  
-  const googleDriveImageLink = "https://drive.google.com/uc?export=view&id=1WaLxDKb-vmlWCRDtV1qkDjyeQIkjDGGC";
 
-  const main_title = "Food you like";
+function Wrapper({loading, webSettings}:{loading: boolean, webSettings: webSettingsProps}) {
 
-  const main_desc = "Food you love";
-  
   return (
-    <div
-      id="bgLanding"
-      className=""
-      style={{
-        zIndex: "1",
-        width: "100%",
-        backgroundImage: `url(${googleDriveImageLink})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="container-fluid px-md-4 pt-md-5">
-        <div className="row pt-md-5">
-          <div className="pt-5 mt-5 px-4">
-            <h1 className="col-8 col-md-12 h1 text-white ps-lg-4 mt-5">
-              <span className="font-boldest-biggest">{main_title}</span>
-            </h1>
-            <p className="pb-md-4 pb-lg-0 mt-md-2 mt-lg-0 text-white ps-lg-4 pt-4">
-              <span className="the-food" style={{ backgroundColor: "black" }}>
-              {main_desc}
-              </span>
-            </p>
+    <>
+      { loading &&
+        <div
+          id="bgLanding"
+          className=""
+          style={{
+            zIndex: "1",
+            width: "100%",
+            backgroundImage: `url(${webSettings.MainPhoto})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="container-fluid px-md-4 pt-md-5">
+            <div className="row pt-md-5">
+              <div className="pt-5 mt-5 px-4">
+                <h1 className="col-8 col-md-12 h1 text-white ps-lg-4 mt-5">
+                  <span className="font-boldest-biggest">{webSettings.MainTitle}</span>
+                </h1>
+                <p className="pb-md-4 pb-lg-0 mt-md-2 mt-lg-0 text-white ps-lg-4 pt-4">
+                  <span className="the-food" style={{ backgroundColor: "black" }}>
+                  {webSettings.MainDesc}
+                  </span>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      }
+    </>
   );
 }
 
--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -3,14 +3,47 @@ import PopularDishes from "../components/Index/PopularDishes";
 import Partners from "../components/Index/Partners";
 import Reviews from "../components/Index/Reviews";
 import Footer from "../components/Index/Footer";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {getWebSettings} from "../helpers/web";
+
+export interface webSettingsProps {
+  MainPhoto: string,
+  MainTitle: string,
+  MainDesc: string,
+  RestaurantDesc: string
+}
 
 function Index() {
+  const [loading, setLoading] = useState(false)
+  const [webSettings, setWebSettings] =
+    useState<webSettingsProps>({MainPhoto: "", MainTitle: "", MainDesc: "", RestaurantDesc: ""})
+
+  const fetchSettings = async () => {
+    setLoading(false)
+    try {
+      const res : any = await getWebSettings()
+      if (res && res.status !== 200) {
+        throw new Error(("Failed to fetch settings."))
+      }
+
+      const result = await res.data.webSettings
+      setWebSettings(result)
+      setLoading(true)
+    } catch (e: any) {
+      console.log(e && e.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchSettings().then()
+  }, []);
+
+
   return (
     <>
-      <Wrapper />
+      <Wrapper loading={loading} webSettings={webSettings} />
       <PopularDishes />
-      <Partners />
+      <Partners loading={loading} webSettings={webSettings}/>
       <Reviews />
       <Footer />
     </>
