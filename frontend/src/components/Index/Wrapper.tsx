f89261d6c6afb46df38b34918f5744588ffae437
Michau-22 <65570834+Michau-22@users.noreply.github.com>
2024-01-08 18:55:28 +0100

    admin (#12)
    start for admin changing photo and desc

--- frontend/src/components/Index/Wrapper.tsx
+++ frontend/src/components/Index/Wrapper.tsx
@@ -2,24 +2,34 @@
 import React from "react";
 
 function Wrapper() {
+  
+  const googleDriveImageLink = "https://drive.google.com/uc?export=view&id=1WaLxDKb-vmlWCRDtV1qkDjyeQIkjDGGC";
+
+  const main_title = "Food you like";
+
+  const main_desc = "Food you love";
+  
   return (
     <div
       id="bgLanding"
       className=""
       style={{
         zIndex: "1",
-        width: "100%"
+        width: "100%",
+        backgroundImage: `url(${googleDriveImageLink})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       <div className="container-fluid px-md-4 pt-md-5">
         <div className="row pt-md-5">
           <div className="pt-5 mt-5 px-4">
             <h1 className="col-8 col-md-12 h1 text-white ps-lg-4 mt-5">
-              <span className="font-boldest-biggest">Food you like.</span>
+              <span className="font-boldest-biggest">{main_title}</span>
             </h1>
             <p className="pb-md-4 pb-lg-0 mt-md-2 mt-lg-0 text-white ps-lg-4 pt-4">
               <span className="the-food" style={{ backgroundColor: "black" }}>
-                Food you love.
+              {main_desc}
               </span>
             </p>
           </div>
